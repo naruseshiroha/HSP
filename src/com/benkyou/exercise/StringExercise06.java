1e1cfa9d9d10be68ad1889c89f8cdeabcc7f6481
鳴瀬　しろは <43293594+naruseshiroha@users.noreply.github.com>
2021-05-07 11:51:28 +0800

    Delete src/com/benkyou directory

--- src/com/benkyou/exercise/StringExercise06.java
+++
@@ -1,9 +0,0 @@
-package com.benkyou.exercise;
-
-public class StringExercise06 {
-    public static void main(String[] args) {
-        // 字符串是不可变的,创建了两个对象
-        String str = "123";
-        str = "1234";
-    }
-}
