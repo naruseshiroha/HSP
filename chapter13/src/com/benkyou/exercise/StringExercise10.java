1a49aa19191bacb8d4e41d3705e429b748bc286f
naruseshiroha <lixunsam@gmail.com>
2021-05-15 11:45:33 +0800

    chapter13 String

--- chapter13/src/com/benkyou/exercise/StringExercise10.java
+++ chapter13/src/com/benkyou/exercise/StringExercise10.java
@@ -1,14 +1,13 @@
 package com.benkyou.exercise;
 
 public class StringExercise10 {
-    String str = new String("linux");
+    String str = new String("hsp");
     final char[] ch = {'j', 'a', 'v', 'a'};
 
     public static void main(String[] args) {
         StringExercise10 ex = new StringExercise10();
-        // 调用方法会新开一个栈
         ex.change(ex.str, ex.ch);
-        System.out.print(ex.str + " and "); // linux and jav_
+        System.out.print(ex.str + " and ");
         System.out.println(ex.ch);
 
     }
